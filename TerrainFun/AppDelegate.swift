73eaa5cd9f176c8529f2e2b93b7d5c860e51b0ce
Rick Mann <rmann@latencyzero.com>
2020-07-22 21:54:04 -0700

    Initial Commit

---
+++ TerrainFun/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TerrainFun
+//
+//  Created by Rick Mann on 2020-07-22.
+//  Copyright © 2020 Latency: Zero, LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ TerrainFun/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TerrainFun
+//
+//  Created by Rick Mann on 2020-07-22.
+//  Copyright © 2020 Latency: Zero, LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
